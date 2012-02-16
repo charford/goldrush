897487c432f56074add2a2bc09f9b7997a69544c
charford <charford@mail.csuchico.edu>
2012-02-15 21:07:52 -0800

    more code fixing

--- test_prg.cpp
+++ test_prg.cpp
@@ -23,16 +23,17 @@ struct mapBoard {
 
 // sets up the shared memory(if not already created), and returns true if 
 // everything goes successful
-int setupGame(mapBoard*& myptr, char currentplayer);
+int setupGame(mapBoard*& myptr, char currentplayer, bool& newgame);
 
 // will be used to check locations on the map, and returns the new player location
-int checkForGold(int location, mapBoard* myptr, char key, Map& goldMine, bool& foundGold);
+int checkForGold(int location, mapBoard* myptr, char key, Map& goldMine, bool& foundGold, char currentplayer);
 
 int main(int argc, char* argv[])
 {
     bool foundGold = false;
+	bool newgame = false;
 	mapBoard* myptr;
-	int p1Location;
+	int p1Location,a;
 	// the current player is determined by the first command line argument(1-4)
 	char currentplayer;
 	if(argc==1) {
@@ -57,53 +58,64 @@ int main(int argc, char* argv[])
 		}
 	}
 	
-	cout << "before" << endl;
-	p1Location = setupGame(myptr,currentplayer);
-    cout << myptr->rows << endl;
-	//cout << "setting up goldmine, " << myptr->board[4] << endl;
+	// setup the game	
+	p1Location = setupGame(myptr,currentplayer,newgame);
+
+	// start the game
 	Map goldMine(myptr->board,myptr->rows,myptr->cols);
-	cout << "goldmine setup done" << endl;
-    int a=0;
-    cout << "p1 x,y = " << p1Location % myptr->cols << ", " << p1Location / myptr->cols << endl;
-    goldMine.postNotice("This is a notice.");
+    goldMine.postNotice("Welcome to GoldRush!");
+
+
     while(true)
     {   
         a=goldMine.getKey();
-        if(a=='Q') break;
+        if(a=='Q') { 
+			myptr->board[p1Location]=0;
+			break;
+		}
         if(a==ERR) goldMine.postNotice("No key pressed");
         //down
         if(a=='j' || a==258) {
-            p1Location=checkForGold(p1Location,myptr, 'j',goldMine,foundGold);
+            p1Location=checkForGold(p1Location,myptr, 'j',goldMine,foundGold,currentplayer);
 	    	goldMine.drawMap();
         }
         //up
         if(a=='k' || a==259) {
-        	p1Location=checkForGold(p1Location,myptr, 'k',goldMine,foundGold);
+        	p1Location=checkForGold(p1Location,myptr, 'k',goldMine,foundGold,currentplayer);
 	    	goldMine.drawMap();
         }
         //left
         if(a=='h' || a==260) {
-    		p1Location=checkForGold(p1Location,myptr, 'h',goldMine,foundGold);
+    		p1Location=checkForGold(p1Location,myptr, 'h',goldMine,foundGold,currentplayer);
             goldMine.drawMap();
         }
         //right
         if(a=='l' || a==261) {
-  			p1Location=checkForGold(p1Location,myptr, 'l',goldMine,foundGold);
+  			p1Location=checkForGold(p1Location,myptr, 'l',goldMine,foundGold,currentplayer);
             goldMine.drawMap();
         }
     }
-    shm_unlink("/casey_goldmine");
+	if(newgame)
+    	shm_unlink("/casey_goldmine");
 }
 
-int setupGame(mapBoard*& myptr, char currentplayer) {
-	cout << "i'm here" << endl;
-	int p1Location;
-    int random;
-    int myRows=0,myCols=0;
-	// open the shared memory
-    int my_file_descriptor;
-    my_file_descriptor=shm_open("/casey_goldmine", O_RDWR|O_CREAT,0600);
+int setupGame(mapBoard*& myptr, char currentplayer, bool& newgame) {
 
+	int p1Location,i,numbergold,random,my_file_descriptor,myRows=0,myCols=0;
+	char* mp;
+ 	const char* ptr;
+    string allLines = "";
+    string oneLine;
+ 
+	// open the shared memory
+    my_file_descriptor=shm_open("/casey_goldmine", O_RDWR,0600);
+	if(my_file_descriptor==-1) {
+		//this is a new game
+		newgame = true;
+    	my_file_descriptor=shm_open("/casey_goldmine", O_RDWR|O_CREAT,0600);
+	}
+
+	// the map file
     ifstream inf("mymap.txt");
 
     if(!inf)
@@ -113,15 +125,12 @@ int setupGame(mapBoard*& myptr, char currentplayer) {
     }
  
     srand(time(NULL));
-    string allLines = "";
-    string oneLine;
 
-    int numbergold;
-    //Remember, the first line is not part
-    //of the map.  Therefore:
+	//get the number of gold to drop(including fools gold)
     getline(inf,oneLine); 
     numbergold = atoi(oneLine.c_str());
-    cout << "first line = " << oneLine << endl;
+
+	//read in each line of map
     while(!getline(inf,oneLine).eof())
     {
         allLines+=oneLine;
@@ -130,54 +139,61 @@ int setupGame(mapBoard*& myptr, char currentplayer) {
         cout << oneLine << endl;
     }
 
-	cout << "about to truncate" << endl;
-    ftruncate(my_file_descriptor,sizeof(mapBoard)+myRows*myCols);
-    lseek(my_file_descriptor,sizeof(mapBoard), SEEK_SET);
-    write(my_file_descriptor, allLines.c_str(), myRows*myCols);
+	if(newgame) {
+		//truncate and write to shared memory
+    	ftruncate(my_file_descriptor,sizeof(mapBoard)+myRows*myCols);
+   		lseek(my_file_descriptor,sizeof(mapBoard), SEEK_SET);
+	    write(my_file_descriptor, allLines.c_str(), myRows*myCols);
+	}
+	
+	//read shared memory, and configure game board rows/cols
     myptr=(mapBoard*)mmap(0,sizeof(mapBoard)+myRows*myCols,PROT_READ|PROT_WRITE,MAP_SHARED,my_file_descriptor,0);
     myptr->rows = myRows;
     myptr->cols = myCols;
-   
-    char* mp=myptr->board;
 
-    //drop the gold
-    while(numbergold>0) {
-        random = rand() % (myptr->rows*myptr->cols) + 1;
-        if(myptr->board[random] == ' ') {
-            if(numbergold==1) myptr->board[random]='G';
-            else myptr->board[random]='F';
-            numbergold--;
-        }
-    }
-	cout << "gold has been dropped" << endl;
-    int i=0; 
-    //drop the players
+	if(newgame) {
+	    //drop the gold randomly
+	    while(numbergold>0) {
+	        random = rand() % (myptr->rows*myptr->cols) + 1;
+	        if(myptr->board[random] == ' ') {
+	            if(numbergold==1) myptr->board[random]='G';
+	            else myptr->board[random]='F';
+	            numbergold--;
+	        }
+	    }
+	}
+
+    i=0; 
+    //drop the player randomly
     while(i<1) {
         random = rand() % (myptr->rows*myptr->cols) + 1;
-        if(myptr->board[random] == ' ') {
+        if(myptr->board[random] != '*') {
             myptr->board[random]=currentplayer;
             p1Location = random;
             i++;
         }
     }
-    const char* ptr = myptr->board;
-    while(*ptr!='\0')
-    {
-        if(*ptr==' ')      *mp=0;
-        else if(*ptr=='*') *mp=G_WALL; //A wall
-        else if(*ptr=='1') *mp=G_PLR0; //The first player
-        else if(*ptr=='2') *mp=G_PLR1; //The second player
-        else if(*ptr=='G') *mp=G_GOLD; //Real gold!
-        else if(*ptr=='F') *mp=G_FOOL; //Fool's gold
-        ++ptr;
-        ++mp;
-    }
-	cout << "game setup done" << endl;
+
+	if(newgame) {
+	    mp=myptr->board;
+	    ptr = myptr->board;
+	    while(*ptr!='\0')
+	    {
+	        if(*ptr==' ')      *mp=0;
+	        else if(*ptr=='*') *mp=G_WALL; //A wall
+	        else if(*ptr=='1') *mp=G_PLR0; //The first player
+	        else if(*ptr=='2') *mp=G_PLR1; //The second player
+	        else if(*ptr=='G') *mp=G_GOLD; //Real gold!
+	        else if(*ptr=='F') *mp=G_FOOL; //Fool's gold
+	        ++ptr;
+	        ++mp;
+	    }
+	}
 	// if it makes it this far, success!
 	return p1Location;
 }
 
-int checkForGold(int location, mapBoard* myptr,char key, Map& goldMine,bool& foundGold) {
+int checkForGold(int location, mapBoard* myptr,char key, Map& goldMine,bool& foundGold,char currentplayer) {
     int x,y;
     //get current location
     x = location % myptr->cols;
@@ -202,7 +218,7 @@ int checkForGold(int location, mapBoard* myptr,char key, Map& goldMine,bool& fou
                     goldMine.postNotice("To bad, that was some fools gold! Try again!");
 				}			
 				myptr->board[location]=0;
-				myptr->board[x+myptr->cols*(y-1)]=G_PLR0;
+				myptr->board[x+myptr->cols*(y-1)]=currentplayer;
 				return x+myptr->cols*(y-1);
 			}
 			break;
@@ -225,7 +241,7 @@ int checkForGold(int location, mapBoard* myptr,char key, Map& goldMine,bool& fou
                     goldMine.postNotice("To bad, that was some fools gold! Try again!");
 				}			
 				myptr->board[location]=0;
-				myptr->board[x+myptr->cols*(y+1)]=G_PLR0;
+				myptr->board[x+myptr->cols*(y+1)]=currentplayer;
 				return x+myptr->cols*(y+1);
 			}
 			break;
@@ -248,7 +264,7 @@ int checkForGold(int location, mapBoard* myptr,char key, Map& goldMine,bool& fou
                     goldMine.postNotice("To bad, that was some fools gold! Try again!");
 				}			
 				myptr->board[location]=0;
-				myptr->board[(x-1)+myptr->cols*y]=G_PLR0;
+				myptr->board[(x-1)+myptr->cols*y]=currentplayer;
 				return (x-1)+myptr->cols*y;
 			}
 			break;
@@ -273,7 +289,7 @@ int checkForGold(int location, mapBoard* myptr,char key, Map& goldMine,bool& fou
 
 				//at this point all checks should be done	
 				myptr->board[location]=0;
-				myptr->board[(x+1)+myptr->cols*y]=G_PLR0;
+				myptr->board[(x+1)+myptr->cols*y]=currentplayer;
 				return (x+1)+myptr->cols*y;
 			}
 			break;
